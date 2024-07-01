ed6c456eeabd7f82d463f6def61a790176e72f62
maury ribeiro <Maurygssss@hotmail.com>
2024-07-01 01:55:54 -0300

    my update

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    window.myeval = function (bodyscript, that, ...args) {
+        // arguments 
+        var func = new Function(bodyscript);
+        return func.call(that, ...args); 
+    }
+})();
