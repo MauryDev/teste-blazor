62633a7dfbc6fdeb3ea66357fb4701b7f9f21270
maury ribeiro <Maurygssss@hotmail.com>
2024-07-02 01:58:50 -0300

    teste23

---
+++ Pages/Label.razor.js
@@ -0,0 +1,16 @@
+﻿export default {
+    InitLabel: function (label, fontsize, color) {
+        if (label != null) {
+            label.style.color = color
+            label.style.fontSize = fontsize
+            label.style.fontFamily = "system-ui"
+        }
+        
+    },
+    UpdateStyle: function (label,stylename, value) {
+        
+        if (label != null) {
+            label.style[stylename] = value
+        }
+    }
+};
